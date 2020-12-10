28851ca148605157d1486467e57c2d6102f6bd48
Kim Sheng Yong <kimshengyong@gmail.com>
2020-12-10 21:14:40 +0800

    add in page navigation in project page

---
+++ frontend/js/project-item.js
@@ -0,0 +1,60 @@
+// (function ($) {
+//   'use strict';
+
+//   jQuery(window).on('load', function () {
+//     // preloader();
+
+//     // if (jQuery('.gallery-outer .gallery-items').length > 0) {
+//     //   jQuery('.gallery-outer .gallery-items').filterizr();
+//     // }
+//     $('.faq-details').hide();
+//     jQuery('.project-item-details li').on('click', function () {
+//       jQuery('.project-item-details li').removeClass('active');
+//       // console.log(this);
+//       jQuery(this).addClass('active');
+//       console.log($('.active').text());
+//       switch ($('.active').text()) {
+//         case 'Campaign':
+//           console.log('campaign');
+//           $('.campaign-details').show();
+//           break;
+//         case 'FAQ':
+//           console.log('faq');
+//           $('.campaign-details').hide();
+//           $('.faq-details').show();
+//       }
+//       // console.log(jQuery(this.innerText));
+//       // if(jQuery(this[0].innerText))
+//     });
+//   });
+// })(jQuery);
+document.getElementById('campaign').click();
+function openCity(evt, cityName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName('tab-content');
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = 'none';
+  }
+  tablinks = document.getElementsByClassName('tablinks');
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(' active', '');
+  }
+  document.getElementById(cityName).style.display = 'block';
+  evt.currentTarget.className += ' active';
+}
+
+//accordion
+var acc = document.getElementsByClassName('accordion');
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener('click', function () {
+    this.classList.toggle('active');
+    var panel = this.nextElementSibling;
+    if (panel.style.display === 'block') {
+      panel.style.display = 'none';
+    } else {
+      panel.style.display = 'block';
+    }
+  });
+}
