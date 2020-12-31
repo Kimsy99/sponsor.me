5086f3c564c89f252d096cd1271379c073e8399b
Kim Sheng Yong <kimshengyong@gmail.com>
2020-12-31 15:12:42 +0800

    add edit profile picture html

---
+++ backend/web/js/edit-profile-pic.js
@@ -0,0 +1,8 @@
+let editProfilePicBox = document.getElementById('insert-profile-picture');
+
+function editProfilePic() {
+    editProfilePicBox.style.display = 'block';
+}
+function closeEdit() {
+    editProfilePicBox.style.display = 'none';
+}
